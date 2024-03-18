b8078823b3818420c878da3085a7c10a7afadd78
Glenn8119 <uuu0622@hotmail.com>
2024-03-18 22:56:32 +0800

    add environment variables

---
+++ facebook_clone/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    host: str
+    password: str
+    port: int
+    username: str
+    database: str
+
+    model_config = SettingsConfigDict(env_file="config.local.env")
+
+
+@lru_cache
+def get_settings():
+    return Settings()
