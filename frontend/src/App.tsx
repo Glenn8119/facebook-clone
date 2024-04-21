d0229cc12aabf96c28a8c0cf0c27067fe5cc38cc
Glenn8119 <uuu0622@hotmail.com>
2024-04-21 15:42:29 +0800

    feat: add path alias

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from './pages/login/Login.tsx'
-import HomePage from './pages/home-page/HomePage.tsx'
+import Login from '@/pages/login/Login.tsx'
+import HomePage from '@/pages/home-page/HomePage.tsx'
 
 const router = createBrowserRouter([
   {
