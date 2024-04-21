7b9fbc91614e7ebe1ab5392bccc449a24bd489a7
Glenn8119 <uuu0622@hotmail.com>
2024-04-21 15:27:38 +0800

    feat: basic login and home page routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,20 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './pages/login/Login.tsx'
+import HomePage from './pages/home-page/HomePage.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  }
+])
+
 function App() {
-  return <div>frontend</div>
+  return <RouterProvider router={router} />
 }
 
 export default App
