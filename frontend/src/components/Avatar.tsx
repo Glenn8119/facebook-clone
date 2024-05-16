279407cb02b1699ddb1379942bd88b524a99f0b8
Glenn8119 <uuu0622@hotmail.com>
2024-05-16 21:19:29 +0800

    FE-feat: mock avatar

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -1,12 +1,13 @@
 import { FC } from 'react'
 import { twMerge } from 'tailwind-merge'
+import FakeAvatar from '@/assets/fake-avatar.jpeg'
 
 type AvatarProps = {
   imgUrl?: string
   className?: string
 }
 
-const Avatar: FC<AvatarProps> = ({ imgUrl, className }) => {
+const Avatar: FC<AvatarProps> = ({ imgUrl = FakeAvatar, className }) => {
   const cn = twMerge('rounded-full', className)
 
   return <img width='40' height='40' src={imgUrl} className={cn} />
