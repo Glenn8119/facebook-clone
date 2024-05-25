6de4161a26b0f1ade6bede806fd246e210046b0c
Glenn8119 <uuu0622@hotmail.com>
2024-05-25 21:41:21 +0800

    BE-modify: recommendation friend api modification

--- backend/facebook_clone/data_access_object/friend.py
+++ backend/facebook_clone/data_access_object/friend.py
@@ -2,9 +2,10 @@ from facebook_clone.data_access_object import BaseDao
 
 
 class FriendDao(BaseDao):
-    async def get_all_user_list(self):
+    async def get_all_user_list(self, excluded_user_id):
         return await self.connection.fetch(
             '''
                 SELECT * FROM user_table
-            '''
+                WHERE NOT id = $1 
+            ''', excluded_user_id
         )
--- backend/facebook_clone/endpoint/friend.py
+++ backend/facebook_clone/endpoint/friend.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from facebook_clone.response import to_json_response
 from typing import List
 from facebook_clone.schema.user import User
+from facebook_clone.depend import depend_user
 from facebook_clone.business_model.friend import FriendBo
 
 
@@ -9,6 +10,6 @@ router = APIRouter()
 
 
 @router.get('/recommendation', response_model=List[User])
-async def get_recommendation_user_list():
-    user_list = await FriendBo().get_recommendation_user_list()
+async def get_recommendation_user_list(user: depend_user):
+    user_list = await FriendBo(user=user).get_recommendation_user_list()
     return to_json_response(user_list)
