7a416d503c190042c92919ca9c7f6bd952ff8b7c
Glenn8119 <uuu0622@hotmail.com>
2024-06-17 20:28:35 +0800

    FE-modify: recommendation friend list style

--- frontend/src/pages/home-page/HomePage.tsx
+++ frontend/src/pages/home-page/HomePage.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
     <div className='flex gap-4'>
       <FunctionList className='min-w-72 basis-1/4 sticky top-14' />
       <PostArea className='basis-1/2' />
-      <RecommendationFriendList className='basis-1/4 h-[calc(100dvh-56px)] p-4 sticky top-14' />
+      <RecommendationFriendList className='min-w-92 basis-1/4 h-[calc(100dvh-56px)] p-4 sticky top-14' />
     </div>
   )
 }
