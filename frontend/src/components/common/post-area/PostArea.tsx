029b7e3a57bb3db7b72b29dc943b9f17ba02cb94
Glenn8119 <uuu0622@hotmail.com>
2024-07-14 10:08:05 +0800

    FE-fix: hide add post functionality when visiting others profile page

--- frontend/src/components/common/post-area/PostArea.tsx
+++ frontend/src/components/common/post-area/PostArea.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 import { twMerge } from 'tailwind-merge'
 import AddPost from '@/components/common/post-area/add-post/AddPost'
 import PostList from '@/components/common/post-area/post-list/PostList'
+import useUserContext from '@/hooks/useUserContext'
 
 type PostAreaProps = {
   className?: string
@@ -9,11 +10,14 @@ type PostAreaProps = {
 }
 
 const PostArea: FC<PostAreaProps> = ({ className, userId }) => {
+  const {
+    value: { id: selfId }
+  } = useUserContext()
   const cn = twMerge('basis-3/5 min-w-125', className)
 
   return (
     <div className={cn}>
-      <AddPost />
+      {userId === selfId ? <AddPost /> : null}
       <PostList userId={userId} />
     </div>
   )
