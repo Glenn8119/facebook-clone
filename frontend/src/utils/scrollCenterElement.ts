1ba7ac6fc54edd6b9968d3aa7e76a9b81e42c38d
Glenn8119 <uuu0622@hotmail.com>
2024-07-18 20:46:04 +0800

    FE-fix: not center the input when comment is clicked

--- frontend/src/utils/scrollCenterElement.ts
+++ frontend/src/utils/scrollCenterElement.ts
@@ -1,5 +1,6 @@
 const scrollCenterElement = <T extends HTMLElement>(el: T) => {
-  const scrollToPositionY = el.offsetTop - window.innerHeight / 2
+  const scrollToPositionY =
+    window.scrollY + el.getBoundingClientRect().top - window.innerHeight / 2
   window.scrollTo({ top: scrollToPositionY, behavior: 'smooth' })
 }
 
